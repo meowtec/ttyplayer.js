387ee24693fc7e84128d424b22d06038e76e6075
Meowtec <bertonzh@gmail.com>
2016-08-20 20:34:07 +0800

    browserify --standalone

--- dist/ttyplay.js
+++ dist/ttyplay.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.TermPlayer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (__dirname){
 /**
  * xterm.js: xterm, in the browser
@@ -5416,192 +5416,191 @@
 
 }).call(this,"/libs")
 },{}],2:[function(require,module,exports){
-;(function() {
-  var Terminal = require('../libs/xterm.js')
-  var EventEmitter = Terminal.EventEmitter
-  var inherits = Terminal.inherits
+var Terminal = require('../libs/xterm.js')
+var EventEmitter = Terminal.EventEmitter
+var inherits = Terminal.inherits
 
-  var hasOwnProperty = Object.prototype.hasOwnProperty
+var hasOwnProperty = Object.prototype.hasOwnProperty
 
-  var defaultCols = 80
-  var defaultRows = 30
+var defaultCols = 80
+var defaultRows = 30
 
-  function extend(a, b) {
-    for (var key in b) {
-      if (hasOwnProperty.call(b, key)) {
-        a[key] = b[key]
+function extend(a, b) {
+  for (var key in b) {
+    if (hasOwnProperty.call(b, key)) {
+      a[key] = b[key]
+    }
+  }
+}
+
+/**
+ * @param {ArrayBuffer} arrayBuffer
+ * @param {number} start
+ * @param {number} length
+ */
+function readUtf8(arrayBuffer, start, length) {
+  return decodeURIComponent(escape(String.fromCharCode.apply(
+    null, new Uint8Array(arrayBuffer, start, length)
+  )))
+}
+
+/**
+ * @param {string} url
+ * @param {function} callback
+ */
+function fetchArrayBuffer(url, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', url, true)
+  xhr.responseType = 'arraybuffer'
+  xhr.send()
+  var error = new Error('XMLHttpRequest error.')
+  xhr.onload = function() {
+    if (!/^2/.test(xhr.status)) {
+      return callback(error)
+    }
+    callback(null, xhr.response)
+  }
+  xhr.onerror = function() {
+    callback(error)
+  }
+}
+
+/**
+ * @param {ArrayBuffer} arrayBuffer
+ */
+function decode(arrayBuffer) {
+  var frames = []
+  var offset = 0
+  var size = arrayBuffer.byteLength
+  var data = new DataView(arrayBuffer)
+
+  while (offset < size) {
+    var sec = data.getUint32(offset, true)
+    offset += 4
+    var usec = data.getUint32(offset, true)
+    offset += 4
+    var length = data.getUint32(offset, true)
+    offset += 4
+
+    frames.push({
+      time: sec * 1000 + usec / 1000,
+      content: readUtf8(arrayBuffer, offset, length)
+    })
+
+    offset += length
+  }
+
+  return frames
+}
+
+function TermPlayer(options) {
+  EventEmitter.call(this)
+
+  var term = new Terminal(options)
+  term.open()
+
+  this.term = term
+}
+
+inherits(TermPlayer, EventEmitter)
+
+extend(TermPlayer.prototype, {
+
+  speed: 1,
+
+  repeat: true,
+
+  interval: 3000,
+
+  play: function(frames) {
+    if (frames) {
+      this.frames = frames
+    }
+    this.term.reset()
+    this.step = 0
+    this.next()
+  },
+
+  pause: function() {
+    clearTimeout(this._nextTimer)
+  },
+
+  resume: function() {
+    this.next()
+  },
+
+  next: function() {
+    var step = this.step
+    var frames = this.frames
+    var currentFrame = frames[step]
+    var nextFrame = frames[step + 1]
+    var str = currentFrame.content
+    var player = this
+    this.term.write(str)
+    this.step = step + 1
+
+    if (nextFrame) {
+      this._nextTimer = setTimeout(
+        function() {
+          player.next()
+        },
+        (nextFrame.time - currentFrame.time) / this.speed
+      )
+    } else if (this.repeat) {
+      setTimeout(function() {
+        player.play()
+      }, this.interval)
+    }
+  },
+
+  load: function(url) {
+    var player = this
+
+    fetchArrayBuffer(url, function(err, data) {
+      if (err) {
+        return player.emit('loadError', err)
       }
-    }
-  }
-
-  /**
-   * @param {ArrayBuffer} arrayBuffer
-   * @param {number} start
-   * @param {number} length
-   */
-  function readUtf8(arrayBuffer, start, length) {
-    return decodeURIComponent(escape(String.fromCharCode.apply(
-      null, new Uint8Array(arrayBuffer, start, length)
-    )))
-  }
-
-  /**
-   * @param {string} url
-   * @param {function} callback
-   */
-  function fetchArrayBuffer(url, callback) {
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', url, true)
-    xhr.responseType = 'arraybuffer'
-    xhr.send()
-    var error = new Error('XMLHttpRequest error.')
-    xhr.onload = function() {
-      if (!/^2/.test(xhr.status)) {
-        return callback(error)
+      var frames
+      try {
+        frames = decode(data)
+      } catch (err) {
+        return player.emit('loadError', err)
       }
-      callback(null, xhr.response)
-    }
-    xhr.onerror = function() {
-      callback(error)
-    }
+
+      player.play(frames)
+    })
   }
+})
 
-  /**
-   * @param {ArrayBuffer} arrayBuffer
-   */
-  function decode(arrayBuffer) {
-    var frames = []
-    var offset = 0
-    var size = arrayBuffer.byteLength
-    var data = new DataView(arrayBuffer)
+TermPlayer.decode = decode
 
-    while (offset < size) {
-      var sec = data.getUint32(offset, true)
-      offset += 4
-      var usec = data.getUint32(offset, true)
-      offset += 4
-      var length = data.getUint32(offset, true)
-      offset += 4
+/**
+ * Usage:
+ *
+ * <div
+ *  data-termplayer-source="./a.rec"
+ *  data-termplayer-cols="120"
+ *  data-termplayer-rows="40"
+ * ></div>
+ *
+ * TermPlayer.initAll()
+ */
+TermPlayer.initAll = function() {
+  var attrSource = 'data-termplayer-source'
+  var targets = document.querySelectorAll('[' + attrSource + ']')
+  var target
 
-      frames.push({
-        time: sec * 1000 + usec / 1000,
-        content: readUtf8(arrayBuffer, offset, length)
-      })
-
-      offset += length
-    }
-
-    return frames
+  for (var i = 0; i < targets.length; i++) {
+    target = targets[i]
+    new TermPlayer({
+      parent: target,
+      cols: parseInt(target.getAttribute('data-termplayer-cols') || defaultCols, 10),
+      rows: parseInt(target.getAttribute('data-termplayer-rows') || defaultRows, 10)
+    }).load(target.getAttribute(attrSource))
   }
+}
 
-  function TermPlayer(options) {
-    EventEmitter.call(this)
+module.exports = TermPlayer
 
-    var term = new Terminal(options)
-    term.open()
-
-    this.term = term
-  }
-
-  inherits(TermPlayer, EventEmitter)
-
-  extend(TermPlayer.prototype, {
-
-    speed: 1,
-
-    repeat: true,
-
-    interval: 2000,
-
-    play: function(frames) {
-      if (frames) {
-        this.frames = frames
-      }
-      this.term.reset()
-      this.step = 0
-      this.next()
-    },
-
-    pause: function() {
-      clearTimeout(this._nextTimer)
-    },
-
-    resume: function() {
-      this.next()
-    },
-
-    next: function() {
-      var step = this.step
-      var frames = this.frames
-      var currentFrame = frames[step]
-      var nextFrame = frames[step + 1]
-      var str = currentFrame.content
-      var player = this
-      this.term.write(str)
-      this.step = step + 1
-
-      if (nextFrame) {
-        this._nextTimer = setTimeout(
-          function() {
-            player.next()
-          },
-          (nextFrame.time - currentFrame.time) / this.speed
-        )
-      } else if (this.repeat) {
-        setTimeout(function() {
-          player.play()
-        }, this.interval)
-      }
-    },
-
-    load: function(url) {
-      var player = this
-
-      fetchArrayBuffer(url, function(err, data) {
-        if (err) {
-          return player.emit('loadError', err)
-        }
-        var frames
-        try {
-          frames = decode(data)
-        } catch (err) {
-          return player.emit('loadError', err)
-        }
-
-        player.play(frames)
-      })
-    }
-  })
-
-  TermPlayer.decode = decode
-
-  /**
-   * Usage:
-   *
-   * <div
-   *  data-termplayer-source="./a.rec"
-   *  data-termplayer-cols="120"
-   *  data-termplayer-rows="40"
-   * ></div>
-   *
-   * TermPlayer.initAll()
-   */
-  TermPlayer.initAll = function() {
-    var attrSource = 'data-termplayer-source'
-    var targets = document.querySelectorAll('[' + attrSource + ']')
-    var target
-
-    for (var i = 0; i < targets.length; i++) {
-      target = targets[i]
-      new TermPlayer({
-        parent: target,
-        cols: parseInt(target.getAttribute('data-termplayer-cols') || defaultCols, 10),
-        rows: parseInt(target.getAttribute('data-termplayer-rows') || defaultRows, 10)
-      }).load(target.getAttribute(attrSource))
-    }
-  }
-
-  window.TermPlayer = TermPlayer
-})()
-
-},{"../libs/xterm.js":1}]},{},[2]);
+},{"../libs/xterm.js":1}]},{},[2])(2)
+});
